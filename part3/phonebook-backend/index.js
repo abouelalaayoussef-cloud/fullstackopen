b223ec26ca31c186c7511915ecd99ce2e5a0584b
YsfAb <abouelalaayoussef@etu.uae.ac.ma>
2026-07-17 13:54:58 +0100

    part3:exercise 3.10 prepare for Render deployment

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,8 +1,15 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
+const PORT = process.env.PORT || 3001
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
 
 app.use(express.json()) 
+app.use(cors())
 
 
  morgan.token('body', (req) => { return JSON.stringify(req.body) })

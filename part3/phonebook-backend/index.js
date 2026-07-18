bc59ec9748b08444ad83fece99cfe4d885275f44
YsfAb <abouelalaayoussef@etu.uae.ac.ma>
2026-07-18 13:02:09 +0100

    part3:Quick fix for deployment

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -4,9 +4,6 @@ const cors = require('cors')
 const app = express()
 const PORT = process.env.PORT || 3001
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
 
 app.use(express.json()) 
 app.use(cors())
@@ -73,8 +70,6 @@ app.post('/api/persons', (req, res) => {
   res.json(newPerson)
 })
 
-
-const PORT = 3001 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })

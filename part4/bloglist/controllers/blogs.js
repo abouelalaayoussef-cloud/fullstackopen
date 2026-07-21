a33b3252e8b02b0b0df72ee69dbd328892a1a766
YsfAb <abouelalaayoussef@etu.uae.ac.ma>
2026-07-21 13:01:40 +0100

    part4: exercises 4.1-4.2 bloglist app initial structure

---
+++ part4/bloglist/controllers/blogs.js
@@ -0,0 +1,19 @@
+const blogsRouter = require('express').Router()
+const Blog = require('../models/blog')
+
+blogsRouter.get('/', async (request, response) => {
+  const blogs = await Blog.find({})
+  response.json(blogs)
+})
+
+blogsRouter.post('/', async (request, response, next) => {
+  const blog = new Blog(request.body)
+
+  blog.save()
+    .then(savedBlog => {
+      response.status(201).json(savedBlog)
+    })
+    .catch(error => next(error))
+})
+
+module.exports = blogsRouter

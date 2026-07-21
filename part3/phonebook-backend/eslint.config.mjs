414cfafe3ebcae01d5bef82a558f5dbf79d4483e
YsfAb <abouelalaayoussef@etu.uae.ac.ma>
2026-07-21 11:55:20 +0100

    part3: exercises 3.19-3.22 validation and ESLint configured

---
+++ part3/phonebook-backend/eslint.config.mjs
@@ -0,0 +1,19 @@
+import js from "@eslint/js";
+import globals from "globals";
+
+export default [
+  {
+    ignores: ["dist", "node_modules"]
+  },
+  js.configs.recommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node
+      }
+    },
+    rules: {
+      'no-console': 'off'
+    }
+  }
+];

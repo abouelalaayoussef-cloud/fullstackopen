a5ef04951982b02f637ed733caae0fd3f11da2e4
YsfAb <abouelalaayoussef@etu.uae.ac.ma>
2026-07-22 10:36:22 +0100

    part4:exercise 4.8 integration tests with supertest

---
+++ part4/bloglist/tests/blog_api.test.js
@@ -0,0 +1,46 @@
+const { test, describe, after, beforeEach } = require('@jest/globals')
+const { expect } = require('@jest/globals')
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const Blog = require('../models/blog')
+
+const api = supertest(app)
+
+const initialBlogs = [
+  {
+    title: 'React patterns',
+    author: 'Michael Chan',
+    url: 'https://reactpatterns.com/',
+    likes: 7
+  },
+  {
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+    likes: 5
+  }
+]
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  await Blog.insertMany(initialBlogs)
+})
+
+describe('when there are initially some blogs saved', () => {
+  test('blogs are returned as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
+
+  test('all blogs are returned', async () => {
+    const response = await api.get('/api/blogs')
+    expect(response.body).toHaveLength(initialBlogs.length)
+  })
+})
+
+afterAll(async () => {
+  await mongoose.connection.close()
+})
